4e97a53158326406407a0accb1084cd3d5ab0a2f
attila <attila@stalphonsos.com>
2015-03-19 14:41:34 -0600

    Excremental work on tbb/OpenBSD.
    Cleaned up the www/tbb ports module a bit.  Not sure this will make
    it through to the final submission.  Fixed the path where extensions
    are dropped after ktrace'ing a tor-browser... irritating.
    
    Added tor-launcher.
    
    Fixed torbutton Makefile after hacking on www/tbb/tbb.port.mk some more.
    
    Add a new patch for tor-browser that stamps out another place where
    they hard-code the location of the user appdir, deep inside the install
    dir, far away from anything that they should be able to write to.

--- tbb/tbb.port.mk
+++ tbb/tbb.port.mk
@@ -3,56 +3,53 @@
 # Module for Tor Browser Bundle (tbb) firefox extensions.
 # Adapted from mail/enigmail/Makefile by attila@stalphonsos.com
 
+MAINTAINER ?=		sean levy <attila@stalphonsos.com>
+
 # keep in synch with mozilla.port.mk
 ONLY_FOR_ARCHS =	i386 amd64 powerpc sparc64
 
-DISTNAME ?=	${TBB_DISTNAME}-${TBB_DISTVERS}
-# ???
-#EPOCH =		0
+DISTNAME ?=		${TBB_DISTNAME}-${TBB_DISTVERS}
 
-HOMEPAGE =	http://www.torproject.org
+HOMEPAGE =		http://www.torproject.org
 
-MASTER_SITES ?=	http://bits.haqistan.net/openbsd-tbb/
+MASTER_SITES ?=		http://bits.haqistan.net/openbsd-tbb/
 
-CATEGORIES =	www
+CATEGORIES =		www
 
 # MPL
 PERMIT_PACKAGE_CDROM=	Yes
 
 CONFIGURE_STYLE =	none
 
-TBB_MAKE_COMMAND ?= ${MAKE_PROGRAM} xpi
-
-BUILD_DEPENDS =	archivers/zip archivers/unzip
-WRKDIST =	${WRKDIR}/${TBB_DISTNAME}-${TBB_DISTVERS}
-
-TBB_BUILDDIR ?= ${WRKBUILD}/${TBB_BUILD_SUBDIR}
+BUILD_DEPENDS =		archivers/zip archivers/unzip
+WRKDIST =		${WRKDIR}/${TBB_DISTNAME}-${TBB_DISTVERS}
+TBB_BUILD_SUBDIR ?=	pkg
+TBB_BUILDDIR ?= 	${WRKBUILD}/${TBB_BUILD_SUBDIR}
 
 # This isn't right: how to keep this synchronized with tor-browser?
-TBB_VERSION ?=	4.0
+TBB_VERSION ?=		4.0
 
 # needed for the naming of the libsubprocess.so
 # the xpi, and the arch matching within mozilla
 .if ${MACHINE_ARCH:Mi386}
-XPCOM_ABI =	x86
+XPCOM_ABI =		x86
 .elif ${MACHINE_ARCH:Mamd64}
-XPCOM_ABI =	x86_64
+XPCOM_ABI =		x86_64
 .elif ${MACHINE_ARCH:Mpowerpc}
-XPCOM_ABI =	ppc
+XPCOM_ABI =		ppc
 .elif ${MACHINE_ARCH:Msparc64}
-XPCOM_ABI =	sparc
+XPCOM_ABI =		sparc
 .endif
-SUBST_VARS += XPCOM_ABI
+SUBST_VARS += 		XPCOM_ABI
 
-EXTDIR ?=	${PREFIX}/lib/tor-browser-${TBB_VERSION}/extensions/
+# Gleaned via ktrace, I don't think this is where it
+# normally goes:
+EXTDIR ?=		${PREFIX}/lib/tor-browser-${TBB_VERSION}/distribution/extensions/
+
 .if !defined(GUID)
 ERRORS += "GUID missing: tbb ports module requires it"
 .endif
-# XXX Is this right?  I took it from the torbutton install.rdf
-APPS = 	{ec8030f7-c20a-464f-9b0e-13a3a9e97384}
-
-do-build:
-	cd ${WRKSRC} && ${TBB_MAKE_COMMAND}
+APPS = 			{ec8030f7-c20a-464f-9b0e-13a3a9e97384}
 
 do-install:
 .for a in ${APPS}
